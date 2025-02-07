354998ef8caacd812827bc34a3231d21a6ed3a16
KavishkaSasindu <sasindu0215@gmail.com>
2025-02-07 20:18:02 +0530

    update the xml file and Laptop Class

--- Spring_Practice/src/main/java/org/example/Laptop.java
+++ Spring_Practice/src/main/java/org/example/Laptop.java
@@ -2,11 +2,21 @@ package org.example;
 
 public class Laptop {
 
+    private int compileTime;
+
     public Laptop() {
         System.out.println("Laptop object is created");
     }
 
     public void compile() {
-        System.out.println("Compiling....");
+        System.out.println("Compiling...." + compileTime);
+    }
+
+    public int getCompileTime() {
+        return compileTime;
+    }
+
+    public void setCompileTime(int compileTime) {
+        this.compileTime = compileTime;
     }
 }
